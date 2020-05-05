feb33751dd408cc6df12996f3455488c44431de7
JhoanRodriguez <jhoansebastian@msn.com>
2020-05-05 21:38:34 +0000

    adding the 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if my_list:
+        new = []
+        for x in my_list:
+            if x % 2 == 0:
+                new.append(True)
+            else:
+                new.append(False)
+    return new
