93490e18686f133ec06b56d8435572d261e6b4dd
JhoanRodriguez <jhoansebastian@msn.com>
2020-05-07 19:57:12 +0000

    fixing the 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-     return {max(a_dictionary) if a_dictionary else None}
+     if a_dictionary
+          return (max(a_dictionary, key=lambda i: a_dictionary[i]))
