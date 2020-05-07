0023264b06fe14458e95df08b72bb742c79fd885
JhoanRodriguez <jhoansebastian@msn.com>
2020-05-07 22:21:03 +0000

    adding the 101

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    return (sum([i * j for i, j in my_list])/sum(j for i, j in my_list))
+    if my_list:
+        return (sum([i * j for i, j in my_list])/sum(j for i, j in my_list))
+    return 0
