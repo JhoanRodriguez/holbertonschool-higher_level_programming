8e9a99de225314cbeea2297dec4ab8c7042dc2e8
JhoanRodriguez <jhoansebastian@msn.com>
2020-05-18 16:32:07 +0000

    adding the 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+def safe_print_list(my_list=[], x=0):
+    counter = 0
+    try:
+        for item in my_list[:x]:
+            print("{}".format(item), end="")
+            counter += 1
+        print()
+    except:
+        print("An error occured, try again")
+    return counter
