5a1cf3c6b00f6062027b1bf2b3f92d37fd291e0d
JhoanRodriguez <jhoansebastian@msn.com>
2020-05-18 19:50:19 +0000

    fixing the \n

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,5 +7,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except Exception as exe:
-        stderr.write("Exception: {}".format(exe))
+        stderr.write("Exception: {}\n".format(exe))
         return False
