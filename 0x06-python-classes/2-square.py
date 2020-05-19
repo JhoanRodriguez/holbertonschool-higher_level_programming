9c08e79c8433fde909c961d3120233b1e4f7f46f
JhoanRodriguez <jhoansebastian@msn.com>
2020-05-19 20:53:02 +0000

    changin the validation for int type

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -16,7 +16,7 @@ class Square:
         """
         if (size < 0):
             raise ValueError("size must be >= 0")
-        elif not isinstance(size, int):
+        elif type(size) is not int:
             raise TypeError("size must be an integer")
         else:
             self.__size = size
