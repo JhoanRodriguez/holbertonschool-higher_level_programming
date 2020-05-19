d1b3d5409892df22cc96d9ca1f7e81904438d28a
JhoanRodriguez <jhoansebastian@msn.com>
2020-05-19 21:14:14 +0000

    adding the 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Class Square"""
+
+
+class Square:
+    """Class Square"""
+    def __init__(self, size=0):
+        """Class Square
+
+        Keyword Arguments:
+            size {int} -- size of square(default: {0})
+
+        Raises:
+            ValueError: must be > 0
+            TypeError: must be an int
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif (value < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """area
+
+        Returns:
+            [int] -- area of square
+        """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """print square"""
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            print("#" * self.__size)
