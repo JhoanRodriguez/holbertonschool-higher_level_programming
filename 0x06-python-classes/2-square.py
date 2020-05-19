fe6aeaaf74e87372e9b2784eddfc960ee548f03f
JhoanRodriguez <jhoansebastian@msn.com>
2020-05-19 21:00:54 +0000

    changin the order of the if statment

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -14,9 +14,9 @@ class Square:
             ValueError: must be > 0
             TypeError: must be an int
         """
-        if (size < 0):
-            raise ValueError("size must be >= 0")
-        elif not isinstance(size, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
