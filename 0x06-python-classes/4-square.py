ad4fe5d84ca5dbe1bbe117ada981514967d8d08e
JhoanRodriguez <jhoansebastian@msn.com>
2020-05-19 20:42:50 +0000

    adding the 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+class Square:
+    """Class Square"""
+    def __init__(self, size=0):
+        """[summary]
+
+        Keyword Arguments:
+            size {int} -- size of square(default: {0})
+
+        Raises:
+            ValueError: must be > 0
+            TypeError: must be an int
+        """
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        elif not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        else:
+            self.__size = size
+
+    def area(self):
+        """area
+
+        Returns:
+            [int] -- area of square
+        """
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
