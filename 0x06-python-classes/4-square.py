fe867620f54742a60acbd3215f341132843a991d
JhoanRodriguez <jhoansebastian@msn.com>
2020-05-19 21:06:47 +0000

    adding a guard condition

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -27,7 +27,12 @@ class Square:
 
     @size.setter
     def size(self, value):
-        self.__size = value
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif (value < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
     def area(self):
         """area
