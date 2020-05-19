64f3c8cae9ba090c3bde6b5c53c6152c8af7ec79
JhoanRodriguez <jhoansebastian@msn.com>
2020-05-19 19:48:13 +0000

    adding the 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    """Class Square"""
+    def __init__(self, size=0):
+        """[summary]
+
+        Keyword Arguments:
+            size {int} -- size of the Square (default: {0})
+        """
+        self.__size = size
