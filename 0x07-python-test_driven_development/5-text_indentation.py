a063e1dc2fc64f53f9b49e382e13ef42a5924273
JhoanRodriguez <jhoansebastian@msn.com>
2020-05-23 14:02:12 +0000

    adding docstring

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""text_indentation"""
+
+
 def text_indentation(text):
+    """identation of the text given
+
+    Arguments:
+        text {[str]} -- text/string to be indented
+
+    Raises:
+        TypeError: text must be a string"
+    """
     if text is None or not isinstance(text, str) or len(text) < 0:
         raise TypeError("text must be a string")
     new_str = "".join([x if x not in ".?:" else x + "\n\n" for x in text])
