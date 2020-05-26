e0af33d7fe83d64a1c40e15d13763ae069b327b1
JhoanRodriguez <jhoansebastian@msn.com>
2020-05-26 01:32:00 +0000

    hot fix

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -14,10 +14,21 @@ class Rectangle:
 
     @property
     def width(self):
+        """Return width if setter checks have passed
+        Returns:
+            int -- width
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Setter validates if value is >= 0
+        Arguments:
+            value {int} -- Value to be setted
+        Raises:
+            TypeError: width must be an integer
+            ValueError: width must be >= 0
+        """
         if type(value) is not int:
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -27,10 +38,21 @@ class Rectangle:
 
     @property
     def height(self):
+        """Return height if setter checks have passed
+        Returns:
+            int -- height
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Setter validates if value is >= 0
+        Arguments:
+            value {int} -- Value to be setted
+        Raises:
+            TypeError: height must be an integer
+            ValueError: height must be >= 0
+        """
         if type(value) is not int:
             raise TypeError("height must be an integer")
         elif value < 0:
