2da49564fa2ea87697ccdb3a3dd75b5efccc62c1
JhoanRodriguez <jhoansebastian@msn.com>
2020-06-02 19:08:28 +0000

    adding the 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""is same class
+    """
+
+
+def is_same_class(obj, a_class):
+    """[summary]
+
+    Arguments:
+        obj class type
+        a_class class type
+
+    Returns:
+        Boolean -- True or false
+    """
+    return (type(obj) == a_class)
