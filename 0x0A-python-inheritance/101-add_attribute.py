b54ae696c26c0e009ad2bccb572258dbbb6bbf68
JhoanRodriguez <jhoansebastian@msn.com>
2020-06-03 03:07:49 +0000

    adding the doc string

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
+"""add_attribute
+    """
+
+
 def add_attribute(ob, attr, value):
+    """add an attribute 
+
+    Arguments:
+        ob dict
+        attr
+        value
+
+    Raises:
+        TypeError: can't add new attribute
+    """
     if hasattr(ob, "__dict__"):
         setattr(ob, attr, value)
     else:
