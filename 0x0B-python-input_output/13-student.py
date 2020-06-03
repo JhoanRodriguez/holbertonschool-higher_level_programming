5b077b4697eecc88552a59236fcdc1bf64dc474c
JhoanRodriguez <jhoansebastian@msn.com>
2020-06-03 23:25:02 +0000

    adding the 13

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+This module creates a class
+student with defined attributes
+"""
+
+
+class Student:
+    """
+    This class is defining the attributes for
+    the said class
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        This is the instantiation of the attributes
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        function returns dict repres
+        of instance
+        """
+        if attrs is None:
+            return (self.__dict__)
+        return ({
+            key: value
+            for key, value in self.__dict__.items() if key in attrs
+        })
+
+    def reload_from_json(self, json):
+        """
+        function that replaces all attrb of the
+        student instance
+        """
+        self.__dict__.update(json)
