639ca2a21974b2a8c5d598b4b1aba01f87aa877b
JhoanRodriguez <jhoansebastian@msn.com>
2020-06-03 22:14:08 +0000

    adding the 9

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+"""
+This file contains a function that adds
+all arguments to a python list and saves
+to a file
+"""
+
+save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
+
+filename = "add_item.json"
+try:
+    new = load_from_json_file(filename)
+except Exception:
+    new = []
+
+new = new + sys.argv[1:]
+save_to_json_file(new, filename)
