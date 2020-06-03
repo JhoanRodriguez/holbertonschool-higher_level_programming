2af23e4e199b837bcdbf05d8bdd00f415ed7dac6
JhoanRodriguez <jhoansebastian@msn.com>
2020-06-03 21:06:55 +0000

    adding the 6

--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 import json
+"""json representation of an object
+    """
 
 
 def to_json_string(my_obj):
+    """
+    function to return JSON
+    representation
+    """
     return json.dumps(my_obj)
