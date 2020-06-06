d38f859072c330422262aa7df19ce546d14d1d7f
JhoanRodriguez <jhoansebastian@msn.com>
2020-06-06 23:39:23 +0000

    Adding the classmethod

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+import json
+
+
 class Base:
     __nb_objects = 0
 
@@ -8,3 +11,22 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None:
+            return []
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        filename = cls.__name__ + ".json"
+        newlist = []
+        if list_objs is None:
+            cls.to_json_string(list_objs)
+
+        with open(filename, "w") as Myfile:
+            for item in list_objs:
+                newlist.append(cls.to_dictionary(item))
+
+            Myfile.write(cls.to_json_string(newlist))
