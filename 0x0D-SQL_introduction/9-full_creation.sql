2aeaf2fd4db72ccb80c5d7bfe9c06504b65a003b
JhoanRodriguez <jhoansebastian@msn.com>
2020-07-13 19:34:00 +0000

    Adding a ' '

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,4 +2,4 @@
 -- in your MySQL server and add multiples rows.
 CREATE TABLE IF NOT EXISTS second_table (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
 name VARCHAR(256), score INT);
-INSERT INTO `second_table` VALUES (NULL, "John", '10'), (NULL, "Alex", '3'), (NULL, "Bob", '14'), (NULL, "George", '8');
+INSERT INTO `second_table` VALUES ('1', "John", '10'), ('2', "Alex", '3'), ('3', "Bob", '14'), ('4', "George", '8');
