55391dfb11345f21cf1a537bc39e6fe13f36540f
JhoanRodriguez <jhoansebastian@msn.com>
2020-07-13 18:52:29 +0000

    Updating the 4

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,4 @@
+-- Write a script that deletes the database hbtn_0c_0 in your MySQL server.
+-- using MYSQL server
+CREATE TABLE IF NOT EXISTS first_table (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+name VARCHAR(256))
