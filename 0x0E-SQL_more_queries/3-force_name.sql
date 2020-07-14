e3985cb78046ae741d3d017a70b57d09b93cabba
JhoanRodriguez <jhoansebastian@msn.com>
2020-07-14 19:24:05 +0000

    Removing autoincrement

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,3 +1,2 @@
 -- Write a script that creates the table force_name on your MySQL server.
-CREATE TABLE IF NOT EXISTS force_name (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
-name VARCHAR(256) NOT NULL)
+CREATE TABLE IF NOT EXISTS force_name (id INT , name VARCHAR(256) NOT NULL)
