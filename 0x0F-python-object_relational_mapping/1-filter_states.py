d171b2dc25321b735775cd91b6ae4de25c4e2cc2
JhoanRodriguez <jhoansebastian@msn.com>
2020-08-09 20:32:55 +0000

    Adding the 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -18,6 +18,7 @@ if __name__ == '__main__':
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
-        print(row)
+        if row[1][0] == "N":
+            print(row)
     cur.close()
     db.close()
