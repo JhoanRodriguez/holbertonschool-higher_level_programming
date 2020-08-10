0a7fc9fc0ef32dd23e19071918d7b354c7202932
JhoanRodriguez <jhoansebastian@msn.com>
2020-08-10 02:48:11 +0000

    Hot fix

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT cities.name FROM cities JOIN states ON state_id = states.id\
-                WHERE states.name = %(sql)s ORDER BY cities.id ASC",
+    cur.execute("""SELECT cities.name FROM cities JOIN states ON state_id = states.id
+                WHERE states.name = %(sql)s ORDER BY cities.id ASC""",
                 {'sql': argv[4]})
     rows = cur.fetchall()
     cities_list = []
