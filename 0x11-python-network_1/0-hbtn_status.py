d2bf7ffc8f77616bb2bf0ea207aac01404f4f0a1
JhoanRodriguez <jhoansebastian@msn.com>
2020-08-24 23:05:05 +0000

    Moving the import

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import urllib.request
 """
 Script that fetches https://intranet.hbtn.io/status
 """
+import urllib.request
 
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
