d8a83917e9bd309f18d1549a5c004396ab06281b
JhoanRodriguez <jhoansebastian@msn.com>
2020-08-24 23:25:37 +0000

    Adding the 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Script that takes in a URL and an email, sends a POST request
+to the passed URL with the email as a parameter, and displays
+the body of the response
+"""
+
+from sys import argv
+import urllib.request as urlreq
+import urllib.parse as urlparse
+
+
+if __name__ == '__main__':
+    url = argv[1]
+    data = urlparse({'email': argv[2]})
+    data = data.encode('ascii')
+
+    with urlreq.urlopen(url, data) as response:
+        print(response.read().decode())
